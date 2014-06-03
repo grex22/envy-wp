00317f12a70ffd87fca12aa4a0928d383afabd3a
Matt Keys <matt@uptrending.com>
2014-06-03 14:41:58 -0400

    fixing URLs in resources sidebar
    Signed-off-by: Matt Keys <matt@uptrending.com>

--- templates/resources-sidebar.php
+++ templates/resources-sidebar.php
@@ -3,9 +3,9 @@
 <?php endif; ?>
 <h2>View by Industry:</h2>
 <ul class="menu">
-	<li class=""><a href="/industry-resource/retail/">Retail</a></li>
-	<li class=""><a href="/industry-resource/restaurant/">Restaurant</a></li>
-	<li class=""><a href="/industry-resource/cinema/">Cinema</a></li>
-	<li class=""><a href="/industry-resource/convenience-store/">Convenience Store</a></li>
-	<li class=""><a href="/industry-resource/hospitality/">Hospitality</a></li>
+	<li class=""><a href="<?php echo home_url('/industry-resource/retail/'); ?>">Retail</a></li>
+	<li class=""><a href="<?php echo home_url('/industry-resource/restaurant/'); ?>">Restaurant</a></li>
+	<li class=""><a href="<?php echo home_url('/industry-resource/cinema/'); ?>">Cinema</a></li>
+	<li class=""><a href="<?php echo home_url('/industry-resource/convenience-store/'); ?>">Convenience Store</a></li>
+	<li class=""><a href="<?php echo home_url('/industry-resource/hospitality/'); ?>">Hospitality</a></li>
 </ul>
