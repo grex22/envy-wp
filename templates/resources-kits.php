5b1175a136e7704d1e2ff57d9c5c148c917f335d
Matt Keys <matt@uptrending.com>
2014-06-04 11:44:49 -0400

    spacing after share button on kits
    Signed-off-by: Matt Keys <matt@uptrending.com>

--- templates/resources-kits.php
+++ templates/resources-kits.php
@@ -7,20 +7,20 @@
       <p><?php the_field('_simple_fields_fieldGroupID_4_fieldID_2_numInSet_0'); ?></p>
       <?php the_content(); ?>
 
-      <p>
-        <?php
-        if( have_rows('marketing_kit_files') ):
+      <br />
+      <br />
+      <?php
+      if( have_rows('marketing_kit_files') ):
 
-          while ( have_rows('marketing_kit_files') ) : the_row();
-            ?>
-            <a href="<?php the_sub_field('file'); ?>" target="_blank" class="cta-btn medium primary"><i class="fa fa-file-pdf-o"></i> <?php the_sub_field('file_name'); ?></a>
-            <p><?php the_sub_field('file_description'); ?></p>
-            <?php
-          endwhile;
-         
-        endif;
-        ?>
-      </p>
+        while ( have_rows('marketing_kit_files') ) : the_row();
+          ?>
+          <a href="<?php the_sub_field('file'); ?>" target="_blank" class="cta-btn medium primary"><i class="fa fa-file-pdf-o"></i> <?php the_sub_field('file_name'); ?></a>
+          <p><?php the_sub_field('file_description'); ?></p>
+          <?php
+        endwhile;
+       
+      endif;
+      ?>
     </div>
     <footer>
       <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
