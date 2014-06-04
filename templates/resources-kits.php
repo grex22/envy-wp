33b18ffce6f75b70f9d225ae908e42d54f64046b
Matt Keys <matt@uptrending.com>
2014-06-04 11:33:32 -0400

    removing bogus pdf link
    Signed-off-by: Matt Keys <matt@uptrending.com>

--- templates/resources-kits.php
+++ templates/resources-kits.php
@@ -7,20 +7,20 @@
       <p><?php the_field('_simple_fields_fieldGroupID_4_fieldID_2_numInSet_0'); ?></p>
       <?php the_content(); ?>
 
-      <?php
-      if( have_rows('marketing_kit_files') ):
+      <p>
+        <?php
+        if( have_rows('marketing_kit_files') ):
 
-        while ( have_rows('marketing_kit_files') ) : the_row();
-          ?>
-          <a href="<?php the_sub_field('file'); ?>" target="_blank" class="cta-btn medium primary"><i class="fa fa-file-pdf-o"></i> <?php the_sub_field('file_name'); ?></a>
-          <p><?php the_sub_field('file_description'); ?></p>
-          <?php
-        endwhile;
-       
-      endif;
-      ?>
-
-      <a href="<?php the_field('_simple_fields_fieldGroupID_3_fieldID_3_numInSet_0'); ?>" target="_blank" class="cta-btn medium primary"><i class="fa fa-file-pdf-o"></i> View PDF</a>
+          while ( have_rows('marketing_kit_files') ) : the_row();
+            ?>
+            <a href="<?php the_sub_field('file'); ?>" target="_blank" class="cta-btn medium primary"><i class="fa fa-file-pdf-o"></i> <?php the_sub_field('file_name'); ?></a>
+            <p><?php the_sub_field('file_description'); ?></p>
+            <?php
+          endwhile;
+         
+        endif;
+        ?>
+      </p>
     </div>
     <footer>
       <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
